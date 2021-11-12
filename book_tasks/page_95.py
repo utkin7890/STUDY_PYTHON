383a178026b49dc3d7c62d8c9096d1599c540062
utkin7890 <utkin7890@gmail.com>
2021-11-12 14:45:52 +0300

    The player gets 5-10-15 points

--- book_tasks/page_95.py
+++ book_tasks/page_95.py
@@ -2,5 +2,12 @@ alien_color = 'red'
 
 if alien_color == 'green':
     print('The player gets 5 points!')
-else:
+
+elif alien_color == 'yellow':
     print('The player gets 10 points!')
+
+elif alien_color == 'red':
+    print('The player gets 15 points!')
+
+else:
+    print('The player does not get points!')
