649c9546ae6bab64af3d2e36bea775dbb4b1a12d
utkin7890 <utkin7890@gmail.com>
2021-11-12 14:54:38 +0300

    The man is teenager

--- page_95_5_6.py
+++ page_95_5_6.py
@@ -1,9 +1,10 @@
-man_age = 2
+man_age = 15
 
 if man_age < 2:
-    print('The man is a baby')
+    print('The man is baby')
 
 if man_age >= 2 and man_age < 4:
-    print('The man is a child')
+    print('The man is child')
 
-
+if man_age >= 13 and man_age < 20:
+    print('The man is teenager')
