a2b5fbda1b79d226f5e16a42541eac0107c72f0f
utkin7890 <utkin7890@gmail.com>
2021-11-15 18:59:59 +0300

    commit description for this script

--- func_names.py
+++ func_names.py
@@ -6,11 +6,8 @@ For this enter a discription of the step from file.feature
 """
 
 while True:
-
     step_name = input('(press q for quit) \n' + 'Input step name for create new name function: ')
-
     new_name_func = step_name.strip().lower().replace(' ', '_')
-
     print('\n \n \n' + new_name_func + '\n \n \n')
 
     if new_name_func == 'q' or new_name_func == 'quit':
