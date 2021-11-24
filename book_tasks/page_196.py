1aa529210be55c2a602bb4727d0da571a5e4edd3
utkin7890 <utkin7890@gmail.com>
2021-11-24 12:06:00 +0300

    ZeroDivisionError  ready

--- book_tasks/page_196.py
+++ book_tasks/page_196.py
@@ -4,12 +4,17 @@ while True:
     a = input("Input first number a= ")
     if a == 'q':
         break
-    b = input("Input second number b= ")
+    b = input("Input first number b= ")
     if b == 'q':
         break
+
     try:
-        divide_res = float(a)/float(b)
+        divide_res = float(a) / float(b)
     except ZeroDivisionError:
         print("\nYou cannot divide by zero!\n")
+    except ValueError:
+        print("\nYou cat enter only numbers!\n")
     else:
         print("\nResult a/b= " + str(divide_res))
+
+
